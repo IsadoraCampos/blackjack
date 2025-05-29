13aa062903cf70e595b224cd3356773832cbcd02
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2025-05-29 09:35:58 -0300

    Update blackjack.asm

--- blackjack.asm
+++ blackjack.asm
@@ -15,14 +15,26 @@
    msgPerdeu: .string "Sua mão estourou! O dealer venceu!\n"
    totalCartas: .word 52
    
-   
 .text
    la a0, msgInicio
    li a7, 4
    ecall
+   call verificaDesejo
+   
+# --- Função: verificaDesejo ---
+#Saída: a0 = 1 (sim) ou 2 (não)
+verificaDesejo:
    la a0, msgDesejaJogar
    li a7, 4
    ecall
    li a7, 5
    ecall
+   li t1, 1 # Para comparar com a resposta do jogador
+   beq a0, t1, jogar
+   li a7, 10 # Se não for 1 encerra o programa
+   ecall
    
+jogar:
+   la a0, msgInicio
+   li a7, 4
+   ecall
