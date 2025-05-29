0f3db54fe9c832d32392f4cf4e3a3d0a01385184
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2025-05-29 09:03:50 -0300

    Update blackjack.asm

--- blackjack.asm
+++ blackjack.asm
@@ -1,7 +1,7 @@
 .data
    asJogador: .word 0
    asCarteador: .word 0
-   msgInicio: .string "Bem-vindo ao BlackJack!"
+   msgInicio: .string "Bem-vindo ao BlackJack!\n"
    msgDesejaJogar: .string "Deseja jogar? (1 - Sim, 2 - Não): "
    msgTotalCartas: .string "Total de cartas: "
    msgPontuacao: .string "Pontuação: "
@@ -11,11 +11,18 @@
    msgJRecebe: .string "O jogador recebe: "
    msgJTem: .string "Sua mão: "
    msgDTem: .string "O dealer tem: "
-   msgVenceu: .string "O dealer estourou! Você venceu!"
-   msgPerdeu: .string "Sua mão estourou! O dealer venceu!"
+   msgVenceu: .string "O dealer estourou! Você venceu!\n"
+   msgPerdeu: .string "Sua mão estourou! O dealer venceu!\n"
+   totalCartas: .word 52
+   
    
 .text
    la a0, msgInicio
    li a7, 4
+   ecall
+   la a0, msgDesejaJogar
+   li a7, 4
+   ecall
+   li a7, 5
    ecall
    
